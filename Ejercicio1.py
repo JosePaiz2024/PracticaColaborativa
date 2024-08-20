9d33dbfd78be606a8a0ffa97ac2c7e529f378ad3
JosePaiz2024 <157830969+JosePaiz2024@users.noreply.github.com>
2024-08-20 10:04:10 -0600

    Ejercicio1

---
+++ Ejercicio1.py
@@ -0,0 +1,15 @@
+class Animal():
+
+    def __init__(self, estado):
+        self.estado = stad
+
+class Perro(Animal):
+    def __init__(self, estado, nombre):
+        super().__init__(estado)
+        self.nombre = name
+
+perrito = Perro("Domestico", "Rex")
+print(perrito)
+
+
+    
